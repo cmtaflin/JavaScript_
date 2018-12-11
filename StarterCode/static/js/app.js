687f51218da462cb844d64199fbcfa16dd505fda
cmtaflin <cmtaflin@gmail.com>
2018-12-11 16:50:31 -0500

    initial load works but not button yet

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,44 +1,86 @@
-// // import data from data.js
-var tableData = data;
 
-var submit = d3.select("#filter-btn");
+// code clears html meta data
+// .html("") to clear any existing metadata
 
-submit.on("click",function(){  
+//Variable can hold values as well as actions
+
+// from data.js
+var sightings = data;
+
+// selcect the location of the table in HTML
+var tbody = d3.select("#tablebody");
+
+// Loop through all records for initial table build
+sightings.forEach((sightings)=>{
   
-  // Function(run_table)
-  console.clear()
-  d3.event.preventDefault();
-
-  var inputElement = d3.select("#datetime.form-control");
-  console.log(inputElement);
-  var inputValue = inputElement.property("value");
-  console.log(inputValue);
-
-  var filterdata = data.filter(data => data.datetime === inputValue);
-  console.log(filterdata);
-
-  var tbody = d3.select("tbody");
-  filterdata.forEach(function(queryData) {
-      // console.log(queryData);
-      var row = tbody.append("tr");
-      Object.entries(queryData).forEach(function([key, value]) {
-        console.log(key, value);
-        var cell = row.append("td");
-        cell.text(value);
-      });
-    });      
+  // Add rows for each record
+  var row = tbody.append("tr");
+      
+  // Append one cell per record
+  Object.entries(sightings).forEach(([key, value]) => {
+  var cell = row.append("td");
+  cell.text(value);
+  })
 });
 
-// function loadTable (data){
-//   var submit = d3.select("#filter-btn").on("click",function());
-  //   d3.event.preventDefault();
-  //   var inputElement = d3.select("#datetime.form-control");
-  //   console.log(inputElement);
-  //   var inputValue = inputElement.property("value");
-  //   console.log(inputValue);
-  //   var filterdata = data.filter(data => data.datetime === inputValue);
-  //   console.log(filterdata);
-  //    
+
+
+
+
+
+
+
+//   function pageReload()
+// {  
+//   function dateFilter()
+//   {
+//     // d3.event.preventdefault()
+//     html(" ")
+//     var inputElement = d3.select("#datetime.form-control");
+//     //console.log(inputElement);
+//     var inputValue = inputElement.property("value");
+//     console.log(inputValue);
+//     var filterdata = data.filter(data => data.datetime === inputValue);	
+//     buildTbl(filterdata);
+//   };
+//   pageReload(tableData);
+// };
+
+
+
+// click_submit.on("click", dateFilter());
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------
+// // OLD that worked with submit button
+// // // import data from data.js
+// var tableData = data;
+
+// var submit = d3.select("#filter-btn");
+
+
+// submit.on("click",function(){  
+  
+
+//   d3.event.preventDefault();
+//   var inputElement = d3.select("#datetime.form-control");
+//   console.log(inputElement);
+//   var inputValue = inputElement.property("value");
+//   console.log(inputValue);
+
+//   var filterdata = data.filter(data => data.datetime === inputValue);
+//   console.log(filterdata);
+
 //   var tbody = d3.select("tbody");
 //   filterdata.forEach(function(queryData) {
 //       // console.log(queryData);
@@ -47,7 +89,19 @@ submit.on("click",function(){
 //         console.log(key, value);
 //         var cell = row.append("td");
 //         cell.text(value);
-//        });          
-//  };  
-// object.onsubmit.on("click",loadTable();  
-// 
+        
+//       });
+//     });      
+// });
+
+
+// function clickFilter()
+// {
+// 	d3.event.preventDefault();
+// 	var filterdata = data.filter(data => data.datetime === inputValue);	
+// 	tbody.html("")
+// 	buildTbl.(filterdata);
+// };
+// d3.select("#filter-btn").on("click",clickFilter());
+
+
