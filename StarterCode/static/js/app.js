b1ae20a5784a9b8069fef9891c6cb01693a2d1db
cmtaflin <cmtaflin@gmail.com>
2018-12-11 21:12:33 -0500

    Finally Done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -23,85 +23,37 @@ sightings.forEach((sightings)=>{
   })
 });
 
+function submitAction(){
 
+  // Prevent page from refreshing
+  d3.event.preventDefault();
 
-
-
-
-
-
-//   function pageReload()
-// {  
-//   function dateFilter()
-//   {
-//     // d3.event.preventdefault()
-//     html(" ")
-//     var inputElement = d3.select("#datetime.form-control");
-//     //console.log(inputElement);
-//     var inputValue = inputElement.property("value");
-//     console.log(inputValue);
-//     var filterdata = data.filter(data => data.datetime === inputValue);	
-//     buildTbl(filterdata);
-//   };
-//   pageReload(tableData);
-// };
-
-
-
-// click_submit.on("click", dateFilter());
-
-
-
-
-
-
-
-
-
-
-
-
-// -------------------------------
-// // OLD that worked with submit button
-// // // import data from data.js
-// var tableData = data;
-
-// var submit = d3.select("#filter-btn");
-
-
-// submit.on("click",function(){  
+  //  Grab input date
+  var inputElement = d3.select("#datetime.form-control");
   
+  var inputValue= inputElement.property("value");
+  console.log(inputValue);
+  
+  // display output in console
+  var filterData = data.filter(ufo => ufo.datetime === inputValue);
+  console.log(filterData);
 
-//   d3.event.preventDefault();
-//   var inputElement = d3.select("#datetime.form-control");
-//   console.log(inputElement);
-//   var inputValue = inputElement.property("value");
-//   console.log(inputValue);
+  // clear prior table  
+  document.getElementById("tablebody").innerHTML="";
 
-//   var filterdata = data.filter(data => data.datetime === inputValue);
-//   console.log(filterdata);
-
-//   var tbody = d3.select("tbody");
-//   filterdata.forEach(function(queryData) {
-//       // console.log(queryData);
-//       var row = tbody.append("tr");
-//       Object.entries(queryData).forEach(function([key, value]) {
-//         console.log(key, value);
-//         var cell = row.append("td");
-//         cell.text(value);
-        
-//       });
-//     });      
-// });
-
-
-// function clickFilter()
-// {
-// 	d3.event.preventDefault();
-// 	var filterdata = data.filter(data => data.datetime === inputValue);	
-// 	tbody.html("")
-// 	buildTbl.(filterdata);
-// };
-// d3.select("#filter-btn").on("click",clickFilter());
+  // Loop through all records for initial table build
+  filterData.forEach(function(queryData){
+    
+    // Add rows for each record
+    var row = tbody.append("tr");
+      
+    // Append one cell per record
+    Object.entries(queryData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    })
+  })
+};
 
 
+d3.select("#filter-btn").on("click",submitAction);
